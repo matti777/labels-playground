a6b39ac122efd5576d79d00af1e9bb4232c975d9
Matti Dahlbom <matti.dahlbom@qvik.fi>
2021-08-12 15:25:30 +0300

    clean up

--- .github/workflows/label_add_in_review.js
+++ .github/workflows/label_add_in_review.js
@@ -1,10 +1,12 @@
 const LABEL_IN_REVIEW = "in review";
 
 module.exports = async ({ github, context }) => {
-  console.log(`CONSOLE: context is: ${JSON.stringify(context)}`);
+  // console.log(`CONSOLE: context is: ${JSON.stringify(context)}`);
 
-  const comment = context.payload.comment;
-  // const issue = context.payload.issue;
+  if (context.payload.action !== "assigned") {
+    return;
+  }
+
   const pr = context.payload.pull_request;
   const repo = context.payload.repository;
 
@@ -15,17 +17,11 @@ module.exports = async ({ github, context }) => {
     !pr.merged &&
     (pr.assignee != null || pr.assignees.length > 0)
   ) {
-    console.log(
-      `ADDING LABEL; PR number: ${pr.number}, repo: ${repo.name}, owner: ${repo.owner.login}`
-    );
     await github.issues.addLabels({
       issue_number: pr.number,
       owner: repo.owner.login,
       repo: repo.name,
       labels: [LABEL_IN_REVIEW],
     });
-  } else {
-    console.log("TODO REMOVE: not adding label");
-    return;
   }
 };
